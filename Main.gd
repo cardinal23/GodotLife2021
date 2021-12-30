0e83a03369f8bf3ad674b423e2e3b244a8b56de2
Mike Dockerty <mike.dockerty@homevalet.co>
2021-12-30 09:53:47 -0600

    Add tick length interaction

--- Main.gd
+++ Main.gd
@@ -1,6 +1,9 @@
 extends Node
 
 var tick = 0
+var tickLengthIndex = 2
+var possibleTickLengths = [0.1, 0.25, 0.5, 1.0]
+
 var clock = 0
 var isPaused := true
 
@@ -22,9 +25,10 @@ func _process(delta):
         return 
         
     tick += delta
+    var tickLength = possibleTickLengths[tickLengthIndex]
     
-    if tick > 1:
-        tick -= 1
+    if tick > tickLength:
+        tick -= tickLength
         clock += 1
         emit_signal("update")
         updateUI()
@@ -36,11 +40,28 @@ func _input(event):
             get_tree().quit()  
         if event.pressed and event.scancode == KEY_SPACE:
             isPaused = !isPaused
+        if event.pressed and event.scancode == KEY_UP:
+            decreaseTickLength()
+        if event.pressed and event.scancode == KEY_DOWN:
+            increaseTickLength()
             
 func updateUI():
     if isPaused:
         clockLabel.text = "Paused"
     else:
         clockLabel.text = String(clock)  
-            
+        
+# Slower ticks    
+func increaseTickLength():
+    tickLengthIndex += 1
+    if tickLengthIndex >= possibleTickLengths.size():
+        tickLengthIndex = possibleTickLengths.size() - 1
+    print("Tick: ", possibleTickLengths[tickLengthIndex], " seconds")    
+        
+# Faster ticks
+func decreaseTickLength():
+    tickLengthIndex -= 1
+    if tickLengthIndex < 0:
+        tickLengthIndex = 0
+    print("Tick: ", possibleTickLengths[tickLengthIndex], " seconds")            
     
